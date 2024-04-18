7d42163c4c82115086be8ea209a402551cfb6707
xueyuejie <358838943@qq.com>
2024-04-18 16:11:23 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/xueyuejie/Asn1BInt.git", branch: "master"),
+        .package(url: "https://github.com/xueyuejie/Asn1BInt.git", from: "1.17.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
